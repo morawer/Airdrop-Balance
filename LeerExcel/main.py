7eda6f4877afb4c5f05f2e9c850cd7ac50b57252
Daniel Morala <morala84@gmail.com>
2021-03-07 16:39:04 +0100

    Prueba de escritura en Excel

--- LeerExcel/main.py
+++ LeerExcel/main.py
@@ -1,10 +1,24 @@
 import pandas as pd
+import xlsxwriter
 
 ex_path = 'airdrop.xlsx'
 df_ex = pd.read_excel(ex_path)
 exchanges = df_ex['address'].values
 exchangesToken = df_ex['tokens'].values
 
+libro = xlsxwriter.Workbook('airdropNew.xlsx')
+sheet = libro.add_worksheet()
+
+row = 0
+col = 0
+
+
+
 for i in range(len(exchanges)):
-    print(exchanges[i], ' >>>>>> ', exchangesToken[i])
+    print(i, ' :::: ', exchanges[i], ' >>>>>> ', exchangesToken[i])
+    sheet.write(i, col, exchanges[i])
+    sheet.write(i, col + 1, exchangesToken[i])
+    sheet.write(i, col + 2, 'Ya se escribir en el Excel')
+
+libro.close()
     
